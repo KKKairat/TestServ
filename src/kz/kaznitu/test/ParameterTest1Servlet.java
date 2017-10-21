2580dcd178701fb91774cee69aaba29a4ae519b4
Kkkairat <rm1697hala@gmail.com>
2017-10-22 01:55:28 +0600

    doPost method kostym

--- src/kz/kaznitu/test/ParameterTest1Servlet.java
+++ src/kz/kaznitu/test/ParameterTest1Servlet.java
@@ -31,4 +31,26 @@ public class ParameterTest1Servlet extends HttpServlet{
         "</html>"
         );
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("test/html");
+        PrintWriter out = resp.getWriter();
+        String title = "Using POST Method to Read Form Data";
+        String docType = "<!doctype html>\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor = \"#f0f0f0\">\n" +
+                "<h1 align = \"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                " <li><b>First Name</b>: "
+                + req.getParameter("first_name") + "\n"+
+                " <li><b>Last Name</b>: "
+                + req.getParameter("last_name") + "\n" +
+                "</ul>\n" +
+                "</body>\n" +
+                "</html>");
+    }
 }
